31e82e68dbd711b6735b979a14da7b95b14cabb6
mrmegabags <chrisjude99@gmail.com>
2025-12-24 16:58:49 +0300

    add authentication

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :sooh_workspace, :scopes,
+  user: [
+    default: true,
+    module: SoohWorkspace.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: SoohWorkspace.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :sooh_workspace,
   ecto_repos: [SoohWorkspace.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
